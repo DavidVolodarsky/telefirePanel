5e502555a6d3fefa8a6d0f510297650f64b2ecc0
David Volodarskyi <davidv@emg-soft.com>
2022-05-12 10:38:15 +0300

    add 'ffi-napi' to devDependency

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // export { Greeter } from '/dist/Greeter';
-// module.exports = require('./dist/MathLibDll');
+
 module.exports = require('./dist/MathLibDll');
 
 // export { Greeter } from './src/index';
@@ -8,3 +8,4 @@ module.exports = require('./dist/MathLibDll');
 //     console.log("This is a message from the demo package");
 //     console.log("DEMO RUN START");
 // }
+
